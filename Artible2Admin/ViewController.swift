5739169760e086aa51f36dab709984c5fa795f93
Nicholas Coggin <nicholascoggin@Nicholass-MacBook-Pro.local>
2019-08-12 06:24:39 -0500

    initial commit

---
+++ Artible2Admin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Artible2Admin
+//
+//  Created by Nicholas Coggin on 8/12/19.
+//  Copyright © 2019 Nicholas Coggin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
